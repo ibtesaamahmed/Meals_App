af59f81bdfd162b0ed869f2e49c38fe1b7f53046
ibtesaamahmed <ibtesaamxhmed007@gmail.com>
2023-07-23 21:03:37 +0500

    Small Fix

--- lib/main.dart
+++ lib/main.dart
@@ -69,6 +69,7 @@ class _MyAppState extends State<MyApp> {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'DeliMeals',
       theme: ThemeData(
         primarySwatch: Colors.green,
